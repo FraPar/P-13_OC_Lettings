222fe3662db2a601133ea8f27bf29d7e9dfd3777
Gaël THOMAS <francois.parenti@gmail.com>
2022-01-14 12:59:48 +0100

    Second step OK, minus deleting old data

--- python_oc_lettings_site/settings.py
+++ python_oc_lettings_site/settings.py
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'python_oc_lettings_site',
+    'lettings',
+    'profiles',
 ]
 
 MIDDLEWARE = [
@@ -38,7 +41,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'oc_lettings_site.urls'
+ROOT_URLCONF = 'python_oc_lettings_site.urls'
 
 TEMPLATES = [
     {
@@ -56,7 +59,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
+WSGI_APPLICATION = 'python_oc_lettings_site.wsgi.application'
 
 
 # Database
---
+++ python_oc_lettings_site/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+
+import lettings.views
+import profiles.views
+import oc_lettings_site.views
+
+urlpatterns = [
+    path('', oc_lettings_site.views.index, name='index'),
+    path('lettings/', lettings.views.index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', lettings.views.letting, name='letting'),
+    path('profiles/', profiles.views.index, name='profiles_index'),
+    path('profiles/<str:username>/', profiles.views.profile, name='profile'),
+    path('admin/', admin.site.urls),
+]

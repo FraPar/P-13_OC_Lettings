403c248c6161e7b22c8f7cc76b88ef4ba71e7caa
Gaël THOMAS <francois.parenti@gmail.com>
2022-01-25 10:47:43 +0100

    changes for Heroku deploy

--- python_oc_lettings_site/settings.py
+++ python_oc_lettings_site/settings.py
@@ -1,7 +1,9 @@
 import os
+from pathlib import Path
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -109,4 +111,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
